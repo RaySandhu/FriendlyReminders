cd955533228c0b272a42df008b71f35ada1d1f2a
Jessy <jessytseng1@gmail.com>
2024-11-16 02:33:33 -0700

    Set up Tag CD

--- lib/services/TagService.dart
+++ lib/services/TagService.dart
@@ -24,15 +24,15 @@ class TagService {
     );
   }
 
-  Future<List<InterestModel>> getTags() async {
-    final db = await _dbClient.database;
-    final List<Map<String, dynamic>> maps = await db.rawQuery('''
-    SELECT I.${_dbClient.interestNameColName}
-    FROM ${_dbClient.interestTagTblName} AS T
-    JOIN ${_dbClient.interestTblName} AS I ON (I.${_dbClient.interestIdColName} = T.${_dbClient.interestIdColName})
-    ''');
-    return List.generate(maps.length, (i) {
-      return InterestModel.fromMap(maps[i]);
-    });
-  }
+  // Future<List<InterestModel>> getTags() async {
+  //   final db = await _dbClient.database;
+  //   final List<Map<String, dynamic>> maps = await db.rawQuery('''
+  //   SELECT I.${_dbClient.interestNameColName}
+  //   FROM ${_dbClient.interestTagTblName} AS T
+  //   JOIN ${_dbClient.interestTblName} AS I ON (I.${_dbClient.interestIdColName} = T.${_dbClient.interestIdColName})
+  //   ''');
+  //   return List.generate(maps.length, (i) {
+  //     return InterestModel.fromMap(maps[i]);
+  //   });
+  // }
 }
