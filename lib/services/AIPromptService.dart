8c77903ab46ffc340ac9addaa37c0e572cc711f5
CarrotBRRR <69835046+CarrotBRRR@users.noreply.github.com>
2024-11-27 21:35:17 -0700

    Added AIPromptsService

---
+++ lib/services/AIPromptService.dart
@@ -0,0 +1,41 @@
+import 'package:friendlyreminder/models/AIPromptModel.dart';
+import 'package:friendlyreminder/services/DatabaseClient.dart';
+
+class AIPromptService {
+  final DatabaseClient _dbClient = DatabaseClient();
+
+  Future<List<AIPromptModel>> getAllPrompts() async {
+    final db = await _dbClient.database;
+    final List<Map<String, dynamic>> maps = await db.query(_dbClient.aiPromptsTbl,
+        orderBy: '${_dbClient.promptId} ASC');
+    return List.generate(maps.length, (i) {
+      return AIPromptModel.fromMap(maps[i]);
+    });
+  }
+
+  Future<AIPromptModel> getPrompt(int promptId) async {
+    final db = await _dbClient.database;
+    final List<Map<String, dynamic>> maps = await db.query(
+      _dbClient.aiPromptTbl,
+      where: '${_dbClient.aiPromptId} = ?',
+      whereArgs: [promptId],
+    );
+    if (maps.isNotEmpty) {
+      return AIPromptModel.fromMap(maps.first);
+    }
+    throw Exception('AIPromptService - Prompt not found');
+  }
+
+  Future<AIPromptModel> getRandomPrompt() async {
+    final db = await _dbClient.database;
+    final List<Map<String, dynamic>> maps = await db.query(
+      _dbClient.aiPromptsTbl,
+      orderBy: 'RANDOM()',
+      limit: 1,
+    );
+    if (maps.isNotEmpty) {
+      return AIPromptModel.fromMap(maps.first);
+    }
+    throw Exception('AIPromptService - No prompts found');
+  }
+}
